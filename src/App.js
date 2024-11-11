c5223e0f86e91f8415511c02a9ea761863bc9b66
t3jehe00 <t3jehe00@students.oamk.fi>
2024-11-11 12:52:13 +0200

    Add local todo project files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,118 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import Row from "./components/Row";
+
+const url = 'http://localhost:3001'
 
 function App() {
+  const [task, setTask] = useState('')
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+
+    axios.get(url)
+
+      .then(response => {
+
+        setTasks(response.data)
+
+      }).catch(error => {
+
+        alert(error.response.data.error ? error.response.data.error : error)
+      })
+
+  }, [])
+
+  /*  const addTask = () => {
+  
+      setTasks([...tasks, task])
+      setTask('')
+    }*/
+  const addTask = () => {
+    axios.post(url + '/create', {
+
+      description: task
+
+    })
+
+      .then(response => {
+
+        setTasks([...tasks, { id: response.data.id, description: task }])
+
+        setTask('')
+
+      }).catch(error => {
+
+        alert(error.response.data.error ? error.response.data.error : error)
+
+      })
+  }
+  /*
+    const deleteTask = (deleted) => {
+  
+      const withoutRemoved = tasks.filter((item) => item !== deleted)
+  
+      setTasks(withoutRemoved)
+  
+    }
+  */
+  const deleteTask = (id) => {
+    axios.delete(url + '/delete/' + id)
+
+      .then(response => {
+
+        const withoutRemoved = tasks.filter((item) => item.id !== id)
+
+        setTasks(withoutRemoved)
+
+      }).catch(error => {
+
+        alert(error.response.data.error ? error.response.data.error : error)
+      })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="container">
+      <h3>Todos</h3>
+      <form>
+        <input
+
+          placeholder='Add new task'
+
+          value={task}
+
+          onChange={e => setTask(e.target.value)}
+
+          onKeyDown={e => {
+
+            if (e.key === 'Enter') {
+
+              e.preventDefault()
+
+              addTask()
+
+            }
+
+          }}
+
+        />
+      </form>
+      <ul>
+        {
+          tasks.map(item => (
+            /*<li key={item.id}>{item.description}
+              <button className='delete-button' onClick={() => deleteTask(item.id)}>Delete</button>
+
+            </li>*/
+            <Row key={item.id} item={item} deleteTask={deleteTask}/>
+
+          ))
+        }
+
+      </ul >
+    </div >
   );
 }
 
